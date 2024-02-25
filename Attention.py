87f60dd6799936804d7895827fc00d821a34f528
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2024-02-25 09:42:15 +0800

    Update Attention.py

--- Attention.py
+++ Attention.py
@@ -8,18 +8,18 @@ import torch.nn.functional as F
 class attention_block(nn.Module):
     def __init__(self, hidden_dim):
         super(attention_block, self).__init__()
-        self.w_omega = nn.Parameter(torch.Tensor(hidden_dim*2, hidden_dim*2))
-        self.u_omega = nn.Parameter(torch.Tensor(hidden_dim*2, 1))
+        self.w_omega = nn.Parameter(torch.Tensor(hidden_dim, hidden_dim))
+        self.u_omega = nn.Parameter(torch.Tensor(hidden_dim, 1))
 
         nn.init.uniform_(self.w_omega, -0.1, 0.1)
         nn.init.uniform_(self.u_omega, -0.1, 0.1)
         
 
     def forward(self, inputs):
-        # inputs = (batch_size, max_words, hidden_dim*2) = (8, 31, 256)
+        # inputs = (batch_size, max_words, hidden_dim) = (8, 31, 256)
 
         u = torch.tanh(torch.matmul(inputs, self.w_omega))
-        # u = (batch_size, max_words, hidden_dim*2)
+        # u = (batch_size, max_words, hidden_dim)
 
         similarity = torch.matmul(u, self.u_omega)
         # similarity = (batch_size, max_words, 1)
@@ -28,7 +28,7 @@ class attention_block(nn.Module):
         # att = (batch_size, max_words, 1)
 
         att_scores = inputs*att
-        # att_scores = (batch_size, max_words, hidden_dim*2)
+        # att_scores = (batch_size, max_words, hidden_dim)
 
         output = torch.sum(att_scores, dim=1)
 

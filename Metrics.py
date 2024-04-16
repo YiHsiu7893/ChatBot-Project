fd0e75adf2434d5a6b389b608d7f3168890e682c
Hsiu <130910974+YiHsiu7893@users.noreply.github.com>
2024-04-16 21:14:23 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -33,8 +33,8 @@ def multi_auc(y, y_pred):
     return auc(fpr["macro"], tpr["macro"])
 
 
-def check_metrics(train_loader, val_loader, model, flag):
-    model.eval()
+def check_metrics(train_loader, val_loader, module, flag):
+    module.b_model.eval()
 
     train_probs = []
     train_predictions = []
@@ -44,25 +44,21 @@ def check_metrics(train_loader, val_loader, model, flag):
     val_labels = []
 
     with torch.no_grad():
-        for x, y in train_loader:
-            x = x.squeeze(1)
-
-            probs = model(x)
+        for inputs, texts, labels in train_loader:
+            probs = module.run(inputs, texts, labels, 'test')
             _, predictions = probs.max(1)
 
             train_probs.extend(probs.numpy())
             train_predictions.extend(predictions.numpy())
-            train_labels.extend(y.numpy())
+            train_labels.extend(labels.numpy())
 
-        for x, y in val_loader:
-            x = x.squeeze(1)
-
-            probs = model(x)
+        for inputs, texts, labels in val_loader:
+            probs = module.run(inputs, texts, labels, 'test')
             _, predictions = probs.max(1)
 
             val_probs.extend(probs.numpy())
             val_predictions.extend(predictions.numpy())
-            val_labels.extend(y.numpy())
+            val_labels.extend(labels.numpy())
 
     print(f"Train       Accuracy: {accuracy_score(train_labels, train_predictions):.2f}")
     print(f"Train       Precision: {precision_score(train_labels, train_predictions, average='macro', zero_division=0):.2f}")
@@ -84,4 +80,4 @@ def check_metrics(train_loader, val_loader, model, flag):
         plt.title(f'Validation Set ROC Curve')
         plt.show()
 
-    model.train()
+    module.b_model.train()

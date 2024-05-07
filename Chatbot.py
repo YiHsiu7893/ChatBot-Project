e5f502e2e0e1505b98026a2430f8bd1c86ae9078
ExplorerRay <adopcarry@gmail.com>
2024-05-07 19:51:43 +0800

    feat: revise code for web api

--- Chatbot.py
+++ Chatbot.py
@@ -6,55 +6,59 @@ from Models import Process_Module
 from Recommender import otc_recmd
 
 
-### Input ###
-#text = input("What symptoms are you experiencing?\n")
-text = "I have a rash on my legs that is causing a lot of discomforts. It seems there is a cramp and I can see prominent veins on the calf. Also, I have been feeling very tired and fatigued in the past couple of days." 
+def main(text):
+    ### Process Module ###
+    # Hyperparameters
+    embedding_dim = 256
+    hidden_dim = 100
+    num_layers = 2
+    class_num = 17
 
 
-### Process Module ###
-# Hyperparameters
-embedding_dim = 256
-hidden_dim = 100
-num_layers = 2
-class_num = 17
+    # Tokenize input sentence
+    sent = tokenizer(text)
+
+    # Load vocabulary list
+    loaded_vocab = torch.load('Weights/vocab.pth')
+
+    # Convert the text to a sequence of word indices
+    sent_indices = [loaded_vocab[word] if word in loaded_vocab else loaded_vocab["UNK"] 
+                    for word in sent]
+
+    # Do padding for same length sequence
+    if len(sent_indices)<31:
+        sent_indices = sent_indices+[0]*(31-len(sent_indices))
 
 
-# Tokenize input sentence
-sent = tokenizer(text)
-
-# Load vocabulary list
-loaded_vocab = torch.load('Weights/vocab.pth')
-
-# Convert the text to a sequence of word indices
-sent_indices = [loaded_vocab[word] if word in loaded_vocab else loaded_vocab["UNK"] 
-                for word in sent]
-
-# Do padding for same length sequence
-if len(sent_indices)<31:
-    sent_indices = sent_indices+[0]*(31-len(sent_indices))
-
-
-# Load pre-trained module
-model = Process_Module(len(loaded_vocab), embedding_dim, hidden_dim, num_layers, class_num, 31) 
-model.load_state_dict(torch.load('Weights/model.pth'))
+    # Load pre-trained module
+    model = Process_Module(len(loaded_vocab), embedding_dim, hidden_dim, num_layers, class_num, 31) 
+    model.load_state_dict(torch.load('Weights/model.pth'))
 
 
 
-### Main ###
-# Make a prediction
-x = torch.tensor(sent_indices).unsqueeze(0)
-text_list = [text]
-probs = model.run(x, text_list, None, 'test')
-_, predictions = probs.max(1)
+    ### Main ###
+    # Make a prediction
+    x = torch.tensor(sent_indices).unsqueeze(0)
+    text_list = [text]
+    probs = model.run(x, text_list, None, 'test')
+    _, predictions = probs.max(1)
 
-idx2dis = torch.load('Weights/idx.pth')
-print(idx2dis[predictions.item()])
+    idx2dis = torch.load('Weights/idx.pth')
+    print(idx2dis[predictions.item()])
+    return idx2dis[predictions.item()]
 
 
-# Medicine recommendation
-otc = input("Do you want to see the OTC recommendations? (press y/Y to get recommendations, otherwise press any key)")
-if otc == 'y' or otc == 'Y':
-    otc_recmd(text)
+if __name__ == '__main__':
+    ### Input ###
+    #text = input("What symptoms are you experiencing?\n")
+    text = "I have a rash on my legs that is causing a lot of discomforts. It seems there is a cramp and I can see prominent veins on the calf. Also, I have been feeling very tired and fatigued in the past couple of days." 
 
-# End of the Chatbot
-print("Thank you for using our service!")
+    main(text)
+
+    # Medicine recommendation
+    otc = input("Do you want to see the OTC recommendations? (press y/Y to get recommendations, otherwise press any key)")
+    if otc == 'y' or otc == 'Y':
+        print(otc_recmd(text))
+
+    # End of the Chatbot
+    print("Thank you for using our service!")
--- Recommender.py
+++ Recommender.py
@@ -51,9 +51,12 @@ def otc_recmd(text):
 
     # Show the result
     print("Top 5 OTC recommendations:")
+    results = ""
     for i in range(5):
         if simi[i][1].item()>0.825:
-            print(otcs[simi[i][0]], simi[i][1].item(), "\n", idcs_zh[simi[i][0]])
+            results += otcs[simi[i][0]] + ", " + str(simi[i][1].item()) + "\n" + idcs_zh[simi[i][0]] + "\n"
+            # print(otcs[simi[i][0]], simi[i][1].item(), "\n", idcs_zh[simi[i][0]])
+    return results
     
 
 #otc_recmd("I have a sore throat and keep coughing. I feel my throat is very dry and I have a fever.")

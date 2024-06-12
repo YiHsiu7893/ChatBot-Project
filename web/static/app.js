e59658981daa8db0632e2128738150dcae15cf8a
ExplorerRay <adopcarry@gmail.com>
2024-06-12 21:27:42 +0800

    feat: async to speed up OTC recommdation

--- web/static/app.js
+++ web/static/app.js
@@ -1,6 +1,7 @@
 var language;
 var inp_dict = {};
 var count = 1;
+var res, drug_result;
 
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
@@ -99,41 +100,69 @@ function sendMsg() {
     chatbox.scrollTo(0, chatbox.scrollHeight);
     data_input = JSON.stringify(inp_dict);
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/func");
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            const msg = JSON.parse(xhr.responseText);
-            if (msg === 'Not enough information') {
-                if (language === 'en') {
-                    incomingChatLi.innerHTML = `<p>Thank you for the description. To assist you more accurately, I need additional information. Please describe your symptoms in detail, including:<br>
-                    •	Duration<br>
-                    •	Severity<br>
-                    •	Any other related symptoms
-                    </p>`;
-                } else {
-                    incomingChatLi.innerHTML = `<p>謝謝您的描述。為了更準確地幫助您，我需要更多的資訊。請詳細描述您的症狀，包括：<br>
-                    - 持續的時間 <br>
-                    - 嚴重程度 <br>
-                    - 是否有其他相關症狀
-                    </p>`;
-                }
-            } else {
-                if (language === 'en') {
-                    incomingChatLi.innerHTML = `<p>The possible disease you have is <strong>${msg}</strong></p>`;
-                    var drugChatLi = createChatLi("To further confirm, you may want to consider seeing a doctor or undergoing additional tests. Would you like to know about over-the-counter medications related to this condition or symptoms?", "chat-incoming");
-                } else {
-                    incomingChatLi.innerHTML = `<p>您可能患有的疾病是<strong>${msg}</strong></p>`;
-                    var drugChatLi = createChatLi("為了進一步確認，您可以考慮去看醫生或進一步檢查。請問您是否想要了解與此疾病或症狀相關的成藥資訊?", "chat-incoming");
-                }
-                chatbox.insertBefore(drugChatLi, opt_elmt);
-                opt_elmt.style.display = "block";
-            }
-            chatbox.scrollTo(0, chatbox.scrollHeight);
+    fetch("/func", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        },
+        body: data_input
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error("Request failed");
         }
+    })
+    .then(msg => {
+        if (msg === 'Not enough information') {
+            if (language === 'en') {
+                incomingChatLi.innerHTML = `<p>Thank you for the description. To assist you more accurately, I need additional information. Please describe your symptoms in detail, including:<br>
+                • Duration<br>
+                • Severity<br>
+                • Any other related symptoms
+                </p>`;
+            } else {
+                incomingChatLi.innerHTML = `<p>謝謝您的描述。為了更準確地幫助您，我需要更多的資訊。請詳細描述您的症狀，包括：<br>
+                - 持續的時間 <br>
+                - 嚴重程度 <br>
+                - 是否有其他相關症狀
+                </p>`;
+            }
+        } else {
+            if (language === 'en') {
+                incomingChatLi.innerHTML = `<p>The possible disease you have is <strong>${msg}</strong></p>`;
+                var drugChatLi = createChatLi("To further confirm, you may want to consider seeing a doctor or undergoing additional tests. Would you like to know about over-the-counter medications related to this condition or symptoms?", "chat-incoming");
+            } else {
+                incomingChatLi.innerHTML = `<p>您可能患有的疾病是<strong>${msg}</strong></p>`;
+                var drugChatLi = createChatLi("為了進一步確認，您可以考慮去看醫生或進一步檢查。請問您是否想要了解與此疾病或症狀相關的成藥資訊?", "chat-incoming");
+            }
+            res = preSendDrug(input, language);
+            chatbox.insertBefore(drugChatLi, opt_elmt);
+            opt_elmt.style.display = "block";
+        }
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+}
+
+async function preSendDrug(input, lang) {
+    const option = 'yes';
+    const data = {
+        option: option,
+        user_input: input,
+        lang: lang
     };
-    xhr.send(data_input);
+
+    return fetch("/drugs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        },
+        body: JSON.stringify(data)
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -217,74 +246,58 @@ document.addEventListener('DOMContentLoaded', function() {
         
         optElmt.style.display = "none";
         chatbox.insertBefore(createChatLi("Yes.", "chat-outgoing"), optElmt);
-        const outgoLis = document.querySelectorAll('.chat-outgoing');
-        const input = outgoLis[outgoLis.length - 2].innerText;
         const incomingChatLi = createChatLi("Thinking...", "chat-incoming");
         chatbox.insertBefore(incomingChatLi, optElmt);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "/drugs");
-        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                const msg = JSON.parse(xhr.responseText);
-                if (Object.keys(msg).length === 0) {
-                    if (lang === 'en') {
-                        incomingChatLi.innerHTML = `<p>Sorry, no over-the-counter drug recommendations found.</p>`;
-                    } else {
-                        incomingChatLi.innerHTML = `<p>抱歉，於我們的資料中，找不到適合的成藥推薦。</p>`;
-                    }
-                } else {
-                    incomingChatLi.remove();
-                    drg.style.display = "block";
-                    if (lang === 'en') {
-                        cpt.innerHTML = `Top ${Object.keys(msg).length} over-the-counter drug recommendations:`;
-                        chatbox.appendChild(createChatLi("The table above is the over-the-counter drug recommendations for your symptoms.<br> However, everyone's situation is different, so be sure to consult your doctor or pharmacist before using any medication.", "chat-incoming"));
-                        chatbox.appendChild(createChatLi("As a reminder, always consult a pharmacist's advice again before purchasing or using any medication to ensure safe usage. Additionally, if your symptoms persist or worsen, seek medical attention promptly. Wishing you a speedy recovery!", "chat-incoming"));
-                    }
-                    else {
-                        cpt.innerHTML = `成藥推薦前 ${Object.keys(msg).length} 名:`;
-                        chatbox.appendChild(createChatLi("上面的表格為針對您症狀的成藥建議。<br>不過，每個人的情況不同，請務必在使用前諮詢您的醫生或藥師。", "chat-incoming"));
-                        chatbox.appendChild(createChatLi("提醒您，在購買或使用任何藥物前，務必再次詢問藥師的建議以確保用藥安全。同時，如果症狀持續或加重，請及時就醫。祝您早日康復！", "chat-incoming"));
-                    }
-                    for (let key in msg) {
-                        var row = drugTb.insertRow();
-                        var rank = row.insertCell(0);
-                        var name = row.insertCell(1);
-                        var simi = row.insertCell(2);
-                        var idc = row.insertCell(3);
-
-                        info = msg[key].split('@');
-
-                        rank.innerHTML = key;
-                        name.innerHTML = info[0];
-                        simi.innerHTML = info[1];
-                        idc.innerHTML = info[2];
-                    }
-                }
-                chatbox.scrollTo(0, chatbox.scrollHeight);
-                sendBtnEn.style.backgroundColor = "red";
-                sendBtnZh.style.backgroundColor = "red";
-                
+        // check if the fetch is done
+        res.then((obj) => obj.json()).then((json) => {
+            drug_result = json;
+            msg = drug_result;
+            if (Object.keys(msg).length === 0) {
                 if (lang === 'en') {
-                    sendBtnEn.innerHTML = "Restart";
+                    incomingChatLi.innerHTML = `<p>Sorry, no over-the-counter drug recommendations found.</p>`;
                 } else {
-                    sendBtnZh.innerHTML = "重新開始";
+                    incomingChatLi.innerHTML = `<p>抱歉，於我們的資料中，找不到適合的成藥推薦。</p>`;
                 }
-                
-                sendBtnEn.onclick = reset;
-                sendBtnZh.onclick = reset;
+            } else {
+                incomingChatLi.remove();
+                drg.style.display = "block";
+                if (lang === 'en') {
+                    cpt.innerHTML = `Top ${Object.keys(msg).length} over-the-counter drug recommendations:`;
+                    chatbox.appendChild(createChatLi("The table above is the over-the-counter drug recommendations for your symptoms.<br> However, everyone's situation is different, so be sure to consult your doctor or pharmacist before using any medication.", "chat-incoming"));
+                    chatbox.appendChild(createChatLi("As a reminder, always consult a pharmacist's advice again before purchasing or using any medication to ensure safe usage. Additionally, if your symptoms persist or worsen, seek medical attention promptly. Wishing you a speedy recovery!", "chat-incoming"));
+                }
+                else {
+                    cpt.innerHTML = `成藥推薦前 ${Object.keys(msg).length} 名:`;
+                    chatbox.appendChild(createChatLi("上面的表格為針對您症狀的成藥建議。<br>不過，每個人的情況不同，請務必在使用前諮詢您的醫生或藥師。", "chat-incoming"));
+                    chatbox.appendChild(createChatLi("提醒您，在購買或使用任何藥物前，務必再次詢問藥師的建議以確保用藥安全。同時，如果症狀持續或加重，請及時就醫。祝您早日康復！", "chat-incoming"));
+                }
+                for (let key in msg) {
+                    var row = drugTb.insertRow();
+                    var rank = row.insertCell(0);
+                    var name = row.insertCell(1);
+                    var simi = row.insertCell(2);
+                    var idc = row.insertCell(3);
+                    info = msg[key].split('@');
+                    rank.innerHTML = key;
+                    name.innerHTML = info[0];
+                    simi.innerHTML = info[1];
+                    idc.innerHTML = info[2];
+                }
             }
-        };
-        
-        const option = 'yes';
-        const data = {
-            option: option,
-            user_input: input,
-            lang: lang
-        };
+            chatbox.scrollTo(0, chatbox.scrollHeight);
+            sendBtnEn.style.backgroundColor = "red";
+            sendBtnZh.style.backgroundColor = "red";
 
-        xhr.send(JSON.stringify(data));
+            if (lang === 'en') {
+                sendBtnEn.innerHTML = "Restart";
+            } else {
+                sendBtnZh.innerHTML = "重新開始";
+            }
+
+            sendBtnEn.onclick = reset;
+            sendBtnZh.onclick = reset;
+        });
     }
 });
